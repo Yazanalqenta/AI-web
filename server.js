4b454e38c9c0a2b2cfa3772b28ffe0fdb5573d1f
Yazanalqenta <141946499+Yazanalqenta@users.noreply.github.com>
2024-10-12 00:39:32 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.json());
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://ai-web-b078.onrender.com/'
+}));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> {
     console.log("Server is active")
